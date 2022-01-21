8e25d9b7d9976d28648aa141e5da813032086105
Michael Dale <vyridian@gmail.com>
2022-01-21 18:30:23 +0900

    Minor product row tweaks

--- frontend/src/views/HomeView.js
+++ frontend/src/views/HomeView.js
@@ -7,7 +7,7 @@ const HomeView = () => {
   return (
     <>
       <h1>Latest Merch</h1>
-      <Row>
+      <Row xs={1} md={2} className="g-4">
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
             <Product product={product} />
