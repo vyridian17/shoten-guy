50fd1102ec4627c271a9155ddd2417696eab3a0e
Michael Dale <vyridian@gmail.com>
2022-01-21 14:20:10 +0900

    Add: header, footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Shoten-Guy!</h1>
+      <Header />
+      <main>
+        <h1>Welcome to Shoten-Guy!</h1>
+      </main>
+      <Footer />
     </>
   );
 };
