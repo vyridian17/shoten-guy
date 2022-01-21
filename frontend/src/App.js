ccef6e2be8b5e917855351d535aa7c9cd27efbf8
Michael Dale <vyridian@gmail.com>
2022-01-21 14:40:52 +0900

    Import bootstrap react css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import "./bootstrap.min.css";
 import "./App.css";
 
 const App = () => {
