7dd7f8602329a1bc97d83c8d576edda453f606df
Michael Dale <vyridian@gmail.com>
2022-01-28 14:48:57 +0900

    Update: fix missing dep var

--- frontend/src/views/ProductView.js
+++ frontend/src/views/ProductView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import {
   Row,
@@ -26,7 +26,7 @@ const ProductView = () => {
       setProduct(data);
     };
     fetchProduct();
-  }, []);
+  }, [id]);
 
   return (
     <>
