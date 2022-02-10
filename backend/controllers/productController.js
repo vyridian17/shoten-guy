4b6bcafc93e838f877e54220027eb82c7a794735
Michael Dale <vyridian@gmail.com>
2022-02-10 21:47:50 +0900

    refactor: routes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,40 @@
+import Product from '../models/productModel.js'
+import mongoose from 'mongoose';
+
+
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
+
+const getProducts = async (req, res) => {
+  const products = await Product.find({}) // MongoDB returns a promise
+  res.json(products);
+}
+
+// @desc Fetch single product
+// @route GET /api/products/:id
+// @access Public
+
+const getProductById = async (req, res) => {
+ if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({
+            message: "Product not found",
+        });
+    }
+  } else {
+    res.status(404).json({
+        message: "Invalid ID. Product not found",
+    });
+  }
+}
+
+export {
+  getProductById,
+  getProducts
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,38 +1,14 @@
 import express from "express";
 const router = express.Router();
-import mongoose from 'mongoose';
-import Product from '../models/productModel.js'
+import {getProducts, getProductById } from '../controllers/productController.js'
 
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
-
-router.get("/", async (_, res) => {
-  const products = await Product.find({}) // MongoDB returns a promise
-  res.json(products);
-});
+router.route("/").get(getProducts);
 
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
 
-router.get("/:id", async (req, res) => {
-  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).json({
-            message: "Product not found",
-        });
-    }
-  } else {
-    res.status(404).json({
-        message: "Invalid ID. Product not found",
-    });
-  }
-})
+router.route("/:id").get(getProductById);
 
 export default router;
