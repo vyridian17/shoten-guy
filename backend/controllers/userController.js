f8979268e0f0e2e646dedb7cd171059dcf430bf0
Michael Dale <vyridian@gmail.com>
2022-02-14 21:42:53 +0900

    add: auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,14 @@
 import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
 import mongoose from 'mongoose';
 import generateToken from '../utils/generateToken.js';
 
 
 // @desc Auth user & get token
-// @route POST /api/products
+// @route POST /api/users
 // @access Public
 
-const authUser = async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
@@ -24,17 +25,64 @@ const authUser = async (req, res) => {
     res.status(401);
     throw new Error('Invalid login')
   }
-}
+})
 
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
 
-const getUserProfile = async (req, res) => {
-  res.send('Success');
-}
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404);
+    throw new Error('User not found.')
+  }
+})
+
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data.')
+  }
+});
 
 export {
   authUser,
-  getUserProfile
+  getUserProfile,
+  registerUser
 }
